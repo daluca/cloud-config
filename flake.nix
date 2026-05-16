f72638d245b064e51b92de8008b91fac5bf36ed9
Lucas Slebos <lucas@slebos.nz>
2026-05-16 15:50:05 +0200

    update format of flake.nix

--- flake.nix
+++ flake.nix
@@ -11,59 +11,74 @@
     treefmt.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, git-hooks, treefmt }:
-  let
-    inherit (nixpkgs) lib;
-    supportedSystems = [ "x86_64-linux" ];
-    forAllSystems = lib.genAttrs supportedSystems;
-    pkgs' = system: nixpkgs.legacyPackages.${system};
-  in {
-    checks = forAllSystems (system:
-      let
-        pkgs = pkgs' system;
-      in {
-        pre-commit = git-hooks.lib.${system}.run {
-          src = ./.;
-          hooks = {
-            check-added-large-files.enable = true;
-            check-merge-conflicts.enable = true;
-            detect-private-keys.enable = true;
-            end-of-file-fixer.enable = true;
-            forbid-new-submodules.enable = true;
-            trim-trailing-whitespace.enable = true;
-            typos.enable = true;
-            deadnix = {
-              enable = true;
-              settings.edit = true;
+  outputs =
+    {
+      self,
+      nixpkgs,
+      git-hooks,
+      treefmt,
+    }:
+    let
+      inherit (nixpkgs) lib;
+      supportedSystems = [ "x86_64-linux" ];
+      forAllSystems = lib.genAttrs supportedSystems;
+      pkgs' = system: nixpkgs.legacyPackages.${system};
+    in
+    {
+      checks = forAllSystems (
+        system:
+        let
+          pkgs = pkgs' system;
+        in
+        {
+          pre-commit = git-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              check-added-large-files.enable = true;
+              check-merge-conflicts.enable = true;
+              detect-private-keys.enable = true;
+              end-of-file-fixer.enable = true;
+              forbid-new-submodules.enable = true;
+              trim-trailing-whitespace.enable = true;
+              typos.enable = true;
+              deadnix = {
+                enable = true;
+                settings.edit = true;
+              };
             };
           };
-        };
-        formatting = (treefmt.lib.evalModule pkgs ./treefmt.nix).config.build.check self;
-      }
-    );
+          formatting = (treefmt.lib.evalModule pkgs ./treefmt.nix).config.build.check self;
+        }
+      );
 
-    formatter = forAllSystems (system:
-      let
-        pkgs = pkgs' system;
-      in
+      formatter = forAllSystems (
+        system:
+        let
+          pkgs = pkgs' system;
+        in
         (treefmt.lib.evalModule pkgs ./treefmt.nix).config.build.wrapper
-    );
+      );
 
-    devShells = forAllSystems (system:
-      let
-        pkgs = pkgs' system;
-      in {
-        default = pkgs.mkShell {
-          inherit (self.checks.${system}.pre-commit) shellHook;
-          name = "cloud-config";
-          packages = with pkgs; self.checks.${system}.pre-commit.enabledPackages ++ [
-            opentofu
-            hcloud
-            just
-          ];
-          JUST_COMMAND_RUNNER = "blue";
-        };
-      }
-    );
-  };
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = pkgs' system;
+        in
+        {
+          default = pkgs.mkShell {
+            inherit (self.checks.${system}.pre-commit) shellHook;
+            name = "cloud-config";
+            packages =
+              with pkgs;
+              self.checks.${system}.pre-commit.enabledPackages
+              ++ [
+                opentofu
+                hcloud
+                just
+              ];
+            JUST_COMMAND_RUNNER = "blue";
+          };
+        }
+      );
+    };
 }
